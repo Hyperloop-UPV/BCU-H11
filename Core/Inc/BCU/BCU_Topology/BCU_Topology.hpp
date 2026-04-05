1e95e3b92c8bb4a2faa4e6471759f6068acabca8
oganigl <osganig@gmail.com>
2026-04-05 20:00:23 +0200

    Added inline to every object from topology and move Board Outside namespac topology, created namespace types

--- Core/Inc/BCU/BCU_Topology/BCU_Topology.hpp
+++ Core/Inc/BCU/BCU_Topology/BCU_Topology.hpp
@@ -1,120 +1,120 @@
 #pragma once
 #include "ST-LIB.hpp"
 #include "BCU/Data/Data.hpp"
-
+#include "BCU/Wrappers/Wrappers.hpp"
 
 namespace BCU {
-    namespace BCU_Topology {
+    namespace Topology {
         //PWM Objects
-        constexpr auto kMotorPhaseR_P = ST_LIB::TimerPin{
+        inline constexpr auto kMotorPhaseR_P = ST_LIB::TimerPin{
            .af = ST_LIB::TimerAF::PWM,
            .pin = Pinout::kPWM_R_P,
            .channel = Pinout::kpwm_channel_R_P,
         };
 
-        constexpr auto kMotorPhaseR_N = ST_LIB::TimerPin{
+        inline constexpr auto kMotorPhaseR_N = ST_LIB::TimerPin{
            .af = ST_LIB::TimerAF::PWM,
            .pin = Pinout::kPWM_R_N,
            .channel = Pinout::kpwm_channel_R_N,
         };
-        constexpr auto kMotorPhaseS_P = ST_LIB::TimerPin{
+        inline constexpr auto kMotorPhaseS_P = ST_LIB::TimerPin{
            .af = ST_LIB::TimerAF::PWM,
            .pin = Pinout::kPWM_S_P,
            .channel = Pinout::kpwm_channel_S_P,
         };
 
-        constexpr auto kMotorPhaseS_N = ST_LIB::TimerPin{
+        inline constexpr auto kMotorPhaseS_N = ST_LIB::TimerPin{
            .af = ST_LIB::TimerAF::PWM,
            .pin = Pinout::kPWM_S_N,
            .channel = Pinout::kpwm_channel_S_N,
         };
 
-        constexpr auto kMotorPhaseT_P = ST_LIB::TimerPin{
+        inline constexpr auto kMotorPhaseT_P = ST_LIB::TimerPin{
            .af = ST_LIB::TimerAF::PWM,
            .pin = Pinout::kPWM_T_P,
            .channel = Pinout::kpwm_channel_T_P,
         };
 
-        constexpr auto kMotorPhaseT_N = ST_LIB::TimerPin{
+        inline constexpr auto kMotorPhaseT_N = ST_LIB::TimerPin{
            .af = ST_LIB::TimerAF::PWM,
            .pin = Pinout::kPWM_T_N,
            .channel = Pinout::kpwm_channel_T_N,
         };
         //timer
-        constexpr auto kmotor_timer = ST_LIB::TimerDomain::Timer(
+        inline constexpr auto kmotor_timer = ST_LIB::TimerDomain::Timer(
             {.request = Pinout::ktimer_PWM},
              kMotorPhaseR_P, kMotorPhaseR_N, kMotorPhaseS_P, kMotorPhaseS_N, kMotorPhaseT_P, kMotorPhaseT_N);
 
         // PPU signals
-        constexpr auto kPPUReady_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kREADY_B);
-        constexpr auto kPPUFault_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kFAULT_B);
-        constexpr auto kPPUReady_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kREADY_A);
-        constexpr auto kPPUFault_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kFAULT_A);
-        constexpr auto kBuffer_Enable = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kBuffer_Enable);
+        inline constexpr auto kPPUReady_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kREADY_B);
+        inline constexpr auto kPPUFault_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kFAULT_B);
+        inline constexpr auto kPPUReady_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kREADY_A);
+        inline constexpr auto kPPUFault_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kFAULT_A);
+        inline constexpr auto kBufferEnable = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kBuffer_Enable);
 
         // LEDs
-        constexpr auto kLED_Operational = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Operational);
-        constexpr auto kLED_Fault = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Fault);
-        constexpr auto kLED_Connecting = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Connecting);
-        constexpr auto kLED_Can = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Can);
-        constexpr auto kLED_Accelerating = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Accelerating);
-        constexpr auto kLED_Braking = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Braking);
+        inline constexpr auto kLED_Operational = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Operational);
+        inline constexpr auto kLED_Fault = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Fault);
+        inline constexpr auto kLED_Connecting = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Connecting);
+        inline constexpr auto kLED_Can = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Can);
+        inline constexpr auto kLED_Accelerating = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Accelerating);
+        inline constexpr auto kLED_Braking = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kLED_Braking);
 
         // Hall sensor supply
-        constexpr auto kHall_Supply_A = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kHall_Supply_A);
-        constexpr auto kHall_Supply_B = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kHall_Supply_B);
+        inline constexpr auto kHall_Supply_A = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kHall_Supply_A);
+        inline constexpr auto kHall_Supply_B = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kHall_Supply_B);
 
         //Hall Sensors
-        constexpr auto kCurrentSenseR_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_R_A);
-        constexpr auto kCurrentSenseR_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_R_B);
-        constexpr auto kCurrentSenseS_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_S_A);
-        constexpr auto kCurrentSenseS_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_S_B);
-        constexpr auto kCurrentSenseT_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_T_A);
-        constexpr auto kCurrentSenseT_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_T_B);
+        inline constexpr auto kCurrentSenseR_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_R_A);
+        inline constexpr auto kCurrentSenseR_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_R_B);
+        inline constexpr auto kCurrentSenseS_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_S_A);
+        inline constexpr auto kCurrentSenseS_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_S_B);
+        inline constexpr auto kCurrentSenseT_A = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_T_A);
+        inline constexpr auto kCurrentSenseT_B = ST_LIB::DigitalInputDomain::DigitalInput(Pinout::kCurrentSense_T_B);
 
         // Speedtec
-        constexpr auto kSpeedtecSupply = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kSpeedtecSupply);
+        inline constexpr auto kSpeedtecSupply = ST_LIB::DigitalOutputDomain::DigitalOutput(Pinout::kSpeedtecSupply);
 
-        constexpr auto kSpeedtec1_Input1 = ST_LIB::TimerPin{
+        inline constexpr auto kSpeedtec1_Input1 = ST_LIB::TimerPin{
             .af = ST_LIB::TimerAF::Encoder,
             .pin = Pinout::kSpeedtec1_IN1,
             .channel = Pinout::kSpeedtec1_Channel_IN1,
         };
-        constexpr auto kSpeedtec1_Input2 = ST_LIB::TimerPin{
+        inline constexpr auto kSpeedtec1_Input2 = ST_LIB::TimerPin{
             .af = ST_LIB::TimerAF::Encoder,
             .pin = Pinout::kSpeedtec1_IN2,
             .channel = Pinout::kSpeedtec1_Channel_IN2
         };
-        constexpr auto kSpeedtec2_Input1 = ST_LIB::TimerPin{
+        inline constexpr auto kSpeedtec2_Input1 = ST_LIB::TimerPin{
             .af = ST_LIB::TimerAF::Encoder,
             .pin = Pinout::kSpeedtec2_IN1,
             .channel = Pinout::kSpeedtec2_Channel_IN1
         };
-        constexpr auto kSpeedtec2_Input2 = ST_LIB::TimerPin{
+        inline constexpr auto kSpeedtec2_Input2 = ST_LIB::TimerPin{
             .af = ST_LIB::TimerAF::Encoder,
             .pin = Pinout::kSpeedtec2_IN2,
             .channel = Pinout::kSpeedtec2_Channel_IN2
         };
 
-        constexpr auto ktimer_speetec1 = ST_LIB::TimerDomain::Timer(
+        inline constexpr auto ktimer_speetec1 = ST_LIB::TimerDomain::Timer(
             {.request = Pinout::ktimer_speetec1},
              kSpeedtec1_Input1, kSpeedtec1_Input2); //Shared timer between speetec 1 and 3
         
-             constexpr auto ktimer_speetec2 = ST_LIB::TimerDomain::Timer(
+             inline constexpr auto ktimer_speetec2 = ST_LIB::TimerDomain::Timer(
             {.request = Pinout::ktimer_speetec2},
              kSpeedtec2_Input1, kSpeedtec2_Input2); 
         // Temperature sensors
-        constexpr auto kTempSensorA = ST_LIB::ADCDomain::ADC(Pinout::kTempSensorA);
-        constexpr auto kTempSensorB = ST_LIB::ADCDomain::ADC(Pinout::kTempSensorB);
+        inline constexpr auto kTempSensorA = ST_LIB::ADCDomain::ADC(Pinout::kTempSensorA);
+        inline constexpr auto kTempSensorB = ST_LIB::ADCDomain::ADC(Pinout::kTempSensorB);
 
         // Voltage sensors
-        constexpr auto kVoltageSensorA = ST_LIB::ADCDomain::ADC(Pinout::kVoltageSensorA);
-        constexpr auto kVoltageSensorB = ST_LIB::ADCDomain::ADC(Pinout::kVoltageSensorB);
+        inline constexpr auto kVoltageSensorA = ST_LIB::ADCDomain::ADC(Pinout::kVoltageSensorA);
+        inline constexpr auto kVoltageSensorB = ST_LIB::ADCDomain::ADC(Pinout::kVoltageSensorB);
 
         // Ethernet (conditional)
         #ifdef STLIB_ETH
         #if defined(USE_PHY_LAN8742)
-        constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
+        inline constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
             ST_LIB::EthernetDomain::PINSET_H10,
             "05:80:e8:55:61:09",
             "192.168.1.5",
@@ -122,14 +122,14 @@ namespace BCU {
         );
         #elif defined(USE_PHY_LAN8700)
         #if MASK_TO_24 == 1
-        constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
+        inline constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
             ST_LIB::EthernetDomain::PINSET_H10,
             "05:80:e8:55:61:09",
             "192.168.1.5",
             "255.255.255.0"
         );
         #else
-        constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
+        inline constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
             ST_LIB::EthernetDomain::PINSET_H10,
             "05:80:e8:55:61:09",
             "192.168.1.5",
@@ -138,14 +138,14 @@ namespace BCU {
         #endif
         #elif defined(USE_PHY_KSZ8041)
         #if MASK_TO_24 == 1
-        constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
+        inline constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
             ST_LIB::EthernetDomain::PINSET_H11,
             "05:80:e8:55:61:09",
             "192.168.1.5",
             "255.255.255.0"
         );
         #else
-        constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
+        inline constexpr auto eth = ST_LIB::EthernetDomain::Ethernet(
             ST_LIB::EthernetDomain::PINSET_H11,
             "05:80:e8:55:61:09",
             "192.168.1.5",
@@ -156,24 +156,27 @@ namespace BCU {
         #error "Ethernet PHY not defined"
         #endif
         #endif
-
-        // Main board register
+    } // namespace BCU_Topology
     using Board = ST_LIB::Board< 
     #ifdef STLIB_ETH
-        eth,
+        Topology::eth,
     #endif
-        kMotorPhaseR_P, kMotorPhaseR_N, kMotorPhaseS_P, kMotorPhaseS_N, kMotorPhaseT_P, kMotorPhaseT_N,
-        kPPUReady_B, kPPUFault_B, kPPUReady_A, kPPUFault_A, kBuffer_Enable,
-        kLED_Operational, kLED_Fault, kLED_Connecting, kLED_Can, kLED_Accelerating, kLED_Braking,
-        kHall_Supply_A, kHall_Supply_B,
-        kCurrentSenseR_A, kCurrentSenseR_B, kCurrentSenseS_A, kCurrentSenseS_B, kCurrentSenseT_A, kCurrentSenseT_B,
-        kSpeedtecSupply,
-        kSpeedtec1_Input1, kSpeedtec1_Input2,
-        kSpeedtec2_Input1, kSpeedtec2_Input2,
-        kTempSensorA, kTempSensorB,
-        kVoltageSensorA, kVoltageSensorB,
-        kmotor_timer,ktimer_speetec1, ktimer_speetec2
+        Topology::kPPUReady_B, Topology::kPPUFault_B, Topology::kPPUReady_A, Topology::kPPUFault_A, Topology::kBufferEnable,
+        Topology::kLED_Operational, Topology::kLED_Fault, Topology::kLED_Connecting, Topology::kLED_Can, Topology::kLED_Accelerating, Topology::kLED_Braking,
+        Topology::kHall_Supply_A, Topology::kHall_Supply_B,
+        Topology::kCurrentSenseR_A, Topology::kCurrentSenseR_B, Topology::kCurrentSenseS_A, Topology::kCurrentSenseS_B, Topology::kCurrentSenseT_A, Topology::kCurrentSenseT_B,
+        Topology::kSpeedtecSupply,
+        Topology::kTempSensorA, Topology::kTempSensorB,
+        Topology::kVoltageSensorA, Topology::kVoltageSensorB,
+        Topology::kmotor_timer,Topology::ktimer_speetec1, Topology::ktimer_speetec2
         >;
-    } // namespace BCU_Topology
+    namespace Types{
+        using MotorTimer = ST_LIB::TimerWrapper<Topology::kmotor_timer>;
+        using PhaseR = decltype(std::declval<MotorTimer>().template get_dual_pwm<Topology::kMotorPhaseR_P, Topology::kMotorPhaseR_N>());
+        using PhaseS = decltype(std::declval<MotorTimer>().template get_dual_pwm<Topology::kMotorPhaseS_P, Topology::kMotorPhaseS_N>());
+        using PhaseT = decltype(std::declval<MotorTimer>().template get_dual_pwm<Topology::kMotorPhaseT_P, Topology::kMotorPhaseT_N>());
+        using BufferEnable = decltype(DigitalOutputWrapper<Board, Types::OLogic::N_CLOSE, Topology::kBufferEnable>());
+    }
+    
 
 } //namespace BCU
