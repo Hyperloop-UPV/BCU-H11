66d96f66a32e942fb1ff18388b491aeda293e513
oganigl <osganig@gmail.com>
2026-04-13 19:15:07 +0200

    modify wrapper namespace to Devices

--- Core/Inc/BCU/Wrappers/Wrappers.hpp
+++ Core/Inc/BCU/Wrappers/Wrappers.hpp
@@ -2,8 +2,14 @@
 #include "ST-LIB.hpp"
 
 
-namespace BCU {
-template <typename Board,Types::OLogic Logic,const auto& DO>
+namespace Devices {
+ //OUTPUT LOGIC (Normally Open or Normally Closed)
+    enum class OLogic: uint8_t{
+        N_OPEN,
+        N_CLOSE,
+    };
+
+template <typename Board,OLogic Logic,const auto& DO>
 class DigitalOutputWrapper {    
     private:
     using DigitalOutput = ST_LIB::DigitalOutputDomain::DigitalOutput;
@@ -13,7 +19,7 @@ class DigitalOutputWrapper {
 
     inline void turn_on() {
         auto& gpio_instance = Board::template instance_of<DO>();
-        if constexpr (Logic == Types::OLogic::N_OPEN) {
+        if constexpr (Logic == OLogic::N_OPEN) {
             gpio_instance.turn_on();
         } else {
             gpio_instance.turn_off();
@@ -21,7 +27,7 @@ class DigitalOutputWrapper {
     }
     inline void turn_off() {
         auto& gpio_instance = Board::template instance_of<DO>();
-        if constexpr (Logic == Types::OLogic::N_CLOSE) {
+        if constexpr (Logic == OLogic::N_OPEN) {
             gpio_instance.turn_off();
         } else {
             gpio_instance.turn_on();
