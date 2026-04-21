4bad5194529a5accf13eb214133931608088c438
oganigl <osganig@gmail.com>
2026-04-22 00:04:36 +0200

    created a Config to change the hardware that I'm using easily

---
+++ Core/Inc/BCU/Config/Config.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define INVERTER 1 // 1 = A / 2 = B / 3 = BOTH
+#define DCLINK 1 // 1 = A / 2 = B / 3 = BOTH
+#define TRIGONOMETRICS_CALC 1 // 1 => Trigonometrics from std / 2 = Cordic / 3 = LUT
+#define SPEETEC 1 // 1 = Use Speetec 1 / 2 = Use Speetec 2 / 3 = Mix
+
