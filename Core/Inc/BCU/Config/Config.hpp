37b7f0cb3dd73c72c2c3b50765079647fcf78f87
oganigl <osganig@gmail.com>
2026-06-04 18:08:30 +0200

    Added configuration to use MATLAB Control

--- Core/Inc/BCU/Config/Config.hpp
+++ Core/Inc/BCU/Config/Config.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #define INVERTER 1            // 1 = A / 2 = B / 3 = BOTH
-#define DCLINK 1              // 1 = A / 2 = B / 3 = BOTH
+#define DCLINK 1              // 1 = A / 2 = B / 3 = BOTH / 4 = Fixed by hand
 #define TRIGONOMETRICS_CALC 1 // 1 => Trigonometrics from std / 2 = Cordic / 3 = LUT
 #define SPEETEC 1             // 1 = Use Speetec 1 / 2 = Use Speetec 2 / 3 = Mix
 
+#define USE_MATLAB_FOC_SPEED 1        // 1 = Use MATLAB FOC / 0 = Use custom FOC
+#define USE_MATLAB_FOC_CURRENT 1      // 1 = Use MATLAB FOC / 0 = Use custom FOC
 // Check the ethernet PHY and define the board version accordingly.
 
 #if defined(USE_PHY_KSZ8041)
